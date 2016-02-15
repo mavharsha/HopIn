1abbb5c7c772a0fb2aca1db64d7a663eb6864837
mavharsha <sreeharsha.mamilla@gmail.com>
2016-02-14 22:05:57 -0800

    Added Stetho to the application

--- app/src/main/java/sk/maverick/harsha/hopin/App.java
+++ app/src/main/java/sk/maverick/harsha/hopin/App.java
@@ -10,6 +10,11 @@ package sk.maverick.harsha.hopin;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
+
 /**
  * Created by Harsha on 2/14/2016.
  */
@@ -17,5 +22,14 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .build());
+
+        new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
     }
 }
